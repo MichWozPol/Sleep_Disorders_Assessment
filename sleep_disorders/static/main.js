2067f7f4c0864a88c9016612a8a89655ef0d6c8e
Michał Sakowicz <michalsakowicz99@gmail.com>
2021-04-14 23:44:20 +0200

    drop down button

---
+++ sleep_disorders/static/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
