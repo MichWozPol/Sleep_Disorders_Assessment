c7b9c0b2a43dfae19d8e318d3ecd6d5712afe089
Michał Sakowicz <michalsakowicz99@gmail.com>
2021-05-17 17:14:58 +0200

    update imports

--- sleep_disorders/settings/db_settings.py
+++ sleep_disorders/settings/db_settings.py
@@ -1,15 +1,9 @@
-import os
 from decouple import config
 
-#secret_key = '5c357453c419ecbb80cd6603f35967f4'
 secret_key = config("secret_key")
-#host = "localhost"
 host = config("host")
-#user = "root"
 user = config("user")
-#password=""
 password = config("password")
-#database="sda_new"
 database = config("database")
 pool_recycle=280
 track_modifications = False
