c8f1f008b47c774d49dc486caa149039d823ff2a
Michał Sakowicz <michalsakowicz99@gmail.com>
2021-05-17 17:14:01 +0200

    add path variable

--- sleep_disorders/settings/db_settings.py
+++ sleep_disorders/settings/db_settings.py
@@ -1,8 +1,16 @@
-secret_key = '5c357453c419ecbb80cd6603f35967f4'
-host = "ocenasnu.mysql.pythonanywhere-services.com"
-user = "ocenasnu"
-password="dbsoa4321"
-database="ocenasnu$survey"
+import os
+from decouple import config
+
+#secret_key = '5c357453c419ecbb80cd6603f35967f4'
+secret_key = config("secret_key")
+#host = "localhost"
+host = config("host")
+#user = "root"
+user = config("user")
+#password=""
+password = config("password")
+#database="sda_new"
+database = config("database")
 pool_recycle=280
 track_modifications = False
 pre_ping = True
