cab181d7dacb18d960b3c4bf7b8f339a14d1f5ba
Michał Sakowicz <michalsakowicz99@gmail.com>
2021-05-20 17:57:18 +0200

    refactor code

--- tools/tree.py
+++ tools/tree.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
@@ -8,41 +9,55 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 import matplotlib.pyplot as plt
 
-path = r"D:\Dokumenty\Studia\SEM6\HIED\votes_weka_80_80.csv"
-df = pd.read_csv(path, error_bad_lines=False)
 
-le = preprocessing.LabelEncoder()
-for column in df.columns:
-    # print(df[column])
-    le.fit(df[column])
-    df[column] = le.transform(df[column])
+def encode_columns(df):
+    le = preprocessing.LabelEncoder()
+    for column in df.columns:
+        # print(df[column])
+        le.fit(df[column])
+        df[column] = le.transform(df[column])
+    return df
 
-# print(le.inverse_transform(df['6']))
-y = df['30']
+def divide_into_X_Y(df):
+    y = df['30']
+    x = df.drop(columns=['30'])
+    return x,y
 
-x = df.drop(columns=['30'])
+def train_test(x,y):
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=10)
+    return X_train, X_test, y_train, y_test
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=10)
+def tree_classifier(X_train, X_test, y_train, y_test):
+    clf = DecisionTreeClassifier(random_state=0)
+    clf.fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+    print(f'Confusion Matrix Tree: \n {metrics.confusion_matrix(y_test, clf.predict(X_test))}')
+    plt.figure(figsize=(30, 30))
+    plot_tree(clf)
+    plt.show()
+    return score
 
-clf = DecisionTreeClassifier(random_state=0)
+def naive_Bayes(X_train, X_test, y_train, y_test):
+    gnb = GaussianNB()
+    gnb.fit(X_train, y_train)
+    scoreNB = gnb.score(X_test, y_test)
+    return scoreNB
 
-clf.fit(X_train, y_train)
+def KNN_Neighbours(X_train, X_test, y_train, y_test):
+    knn = KNeighborsClassifier(n_neighbors=3)
+    knn.fit(X_train, y_train)
+    score_knn = knn.score(X_test, y_test)
+    return score_knn
 
-score = clf.score(X_test, y_test)
 
-gnb = GaussianNB()
+if __name__ == "__main__":
+    path = r"C:\Users\micha\OneDrive\Documents\Programowanie\Python\Sleep_Disorders_Assessment\project_resources\prezentacja 3\with_quotes.csv"
+    df = pd.read_csv(path, error_bad_lines=False)
+    df = encode_columns(df)
+    x,y = divide_into_X_Y(df)
+    X_train, X_test, y_train, y_test = train_test_split(x,y)
 
-gnb.fit(X_train, y_train)
-
-scoreNB = gnb.score(X_test, y_test)
-
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(X_train, y_train)
-score_knn = knn.score(X_test, y_test)
-
-print(f'Tree: {score}, NB: {scoreNB}, KNN: {score_knn}')
-print(f'Confusion Matrix Tree: \n {metrics.confusion_matrix(y_test, clf.predict(X_test))}')
-
-plt.figure(figsize=(30, 30))
-plot_tree(clf)
-plt.show()
+    score_tree = tree_classifier(X_train, X_test, y_train, y_test)
+    score_nb = naive_Bayes(X_train, X_test, y_train, y_test)
+    score_knn = KNN_Neighbours(X_train, X_test, y_train, y_test)
+    print(f'Tree: {score_tree}, NB: {score_nb}, KNN: {score_knn}')
